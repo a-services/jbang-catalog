4c0744b04021cce3012452c2df5a4037571170f8
Egor Abramovich <egor.abramovich@gmail.com>
2021-11-19 13:46:51 +0300

    csv

--- csv_from_db.java
+++ csv_from_db.java
@@ -2,6 +2,7 @@
 //DEPS org.apache.commons:commons-csv:1.9.0
 //DEPS info.picocli:picocli:4.6.2
 //DEPS org.postgresql:postgresql:42.2.4
+//DEPS org.json:json:20180130
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -10,10 +11,16 @@ import picocli.CommandLine.Option;
 import static java.lang.System.*;
 
 import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.*;
+import java.util.Arrays;
 import java.util.concurrent.Callable;
 
 import org.apache.commons.csv.*;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 /**
  * Export CSV file from database table.
@@ -36,17 +43,66 @@ public class csv_from_db implements Callable<Integer> {
     @Option(names = { "-f", "--file" }, required = true, description = "Output file")
     private String outFile;
 
+    String[] outFormats = { "csv", "json" };
+
     @Override
     public Integer call() throws Exception {
+        out.println("Output file: " + outFile);
+        int outFormat = determineOutputFormat(outFile);
+        if (outFormat == -1) {
+            out.println("[ERROR] Unknown output format.");
+            return 1;
+        }
+
+        /* Export result set
+         */
         try (Connection con = DriverManager.getConnection("jdbc:postgresql:" + dbName, username, password)) {
             Statement stmt = con.createStatement();
             ResultSet rs = stmt.executeQuery("SELECT * FROM " + tableName);
 
-            try (CSVPrinter printer = new CSVPrinter(new FileWriter(outFile), CSVFormat.DEFAULT)) {
-                printer.printRecords(rs);
+            switch (outFormat) {
+                case 0:
+                    exportCsv(rs);
+                    break;
+                case 1:
+                    exportJson(rs);
+                    break;
             }
         }
         return 0;
+    }
+
+    private void exportJson(ResultSet rs) throws SQLException, IOException {
+        JSONArray json = new JSONArray();
+        ResultSetMetaData rsmd = rs.getMetaData();
+        while (rs.next()) {
+            int numColumns = rsmd.getColumnCount();
+            JSONObject obj = new JSONObject();
+            for (int i = 1; i <= numColumns; i++) {
+                String column_name = rsmd.getColumnName(i);
+                obj.put(column_name, rs.getObject(column_name));
+            }
+            json.put(obj);
+        }
+        Files.writeString(Paths.get(outFile), json.toString());
+    }
+
+    private void exportCsv(ResultSet rs) throws SQLException, IOException {
+        try (CSVPrinter printer = new CSVPrinter(new FileWriter(outFile), CSVFormat.DEFAULT)) {
+            printer.printRecords(rs);
+        }
+    }
+
+    /**
+     * Determine output format by file extension
+     */
+    private int determineOutputFormat(String outFileName) {
+        int k = outFileName.lastIndexOf(".");
+        if (k == -1) {
+            return -1;
+        }
+        String ext = outFileName.substring(k + 1);
+        return Arrays.asList(outFormats).indexOf(ext);
     }
 
     public static void main(String... args) {
