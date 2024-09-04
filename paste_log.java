1ed6d9de123d09dc70717dacbcba1ecfe41e42fc
Egor Abramovich <egor.abramovich@gmail.com>
2024-09-04 11:39:45 +0300

    paste_log

--- paste_log.java
+++ paste_log.java
@@ -1,5 +1,5 @@
 ///usr/bin/env jbang "$0" "$@" ; exit $?
-//DEPS info.picocli:picocli:4.7.0
+//DEPS info.picocli:picocli:4.7.6
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
@@ -10,19 +10,27 @@ import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.UnsupportedFlavorException;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
+import javax.swing.JOptionPane;
+import java.util.Properties;
+
 // https://github.com/a-services/jbang-catalog/blob/main/paste_log.java
 
-@Command(name = "paste_log", mixinStandardHelpOptions = true, version = "20-11-09", 
+@Command(name = "paste_log", mixinStandardHelpOptions = true, version = "2024-09-04", 
          description = "Paste log from clipboard to file")
 class paste_log implements Callable<Integer> {
 
+    final String propFileName = ".paste_log";
+    
+    Properties pp = new Properties();
+    
     @Override
     public Integer call() throws Exception {
         String text = paste();
@@ -30,8 +38,20 @@ class paste_log implements Callable<Integer> {
             System.out.println("[WARNING] Empty clipboard");
             return 1;
         }
+        
+        String prefix = loadPrefix();
+        prefix = JOptionPane.showInputDialog("Prefix?", prefix);
+        if (prefix != null) {
+            prefix = prefix.trim();
+            savePrefix(prefix);
+        }
+        
         LocalDateTime today = LocalDateTime.now();
         String fname = today.format(DateTimeFormatter.ofPattern("yyyy-MM-dd-HH-mm")) + ".log";
+        if (prefix != null) {
+            fname = prefix + "-" + fname;
+        }
+        
         saveStr(fname, text);
         System.out.println("File created: " + fname);
         return 0;
@@ -52,6 +72,28 @@ class paste_log implements Callable<Integer> {
             }
         }
         return null;
+    }
+    
+    String loadPrefix() throws IOException {
+        String prefix = null;
+        
+        File f = new File(propFileName);
+        if (f.exists()) {
+            FileInputStream fin = new FileInputStream(f.getPath());
+            pp.load(fin);
+            fin.close();
+            
+            prefix = pp.getProperty("prefix");
+        }
+        return prefix;
+    }
+    
+    void savePrefix(String prefix) throws IOException {
+        pp.setProperty("prefix", prefix);
+        
+        FileOutputStream out = new FileOutputStream(propFileName);
+        pp.store(out, null);
+        out.close();
     }
     
     /** Save string to file. */
