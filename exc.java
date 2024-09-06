fbe9adf7d681fad0804c8ea0484fddc0f423121f
Egor Abramovich <egor.abramovich@gmail.com>
2024-09-06 12:07:33 +0300

    exc

--- exc.java
+++ exc.java
@@ -35,9 +35,15 @@ import picocli.CommandLine.Command;
 import picocli.CommandLine.Option;
 import picocli.CommandLine.Parameters;
 
-
+/**
+ * Command-line application for checking exceptions in a log file.
+ * <p>
+ * This application processes a specified log file to extract exceptions and output them in various formats,
+ * including YAML and HTML. The user can specify options for timestamp format, encoding, and other parameters.
+ * </p>
+ */
 @Command(name = "exc", mixinStandardHelpOptions = true, version = "2024-04-05", 
-         description = "Checking for exceptions in log file")
+         description = "Checking exceptions in a log file")
 class exc implements Callable<Integer> {
 
     @Parameters(index = "0", description = "Log file")
@@ -53,17 +59,28 @@ class exc implements Callable<Integer> {
     @Option(names = { "--html" }, description = "Output HTML file.")
     boolean outputHtml;
 
+    @Option(names = { "-r", "--reverse" }, description = "Reverse order.")
+    boolean reverseOrder;
+
     @Option(names = { "--encoding" }, description = "Log file encoding.")
     String encoding = "UTF-8";
     
     @Option(names = { "--skip" }, description = "Skip prefix length.")
     int skipPrefix = 0;
 
+    @Option(names = { "--restart" }, description = "Restart signature.")
+    String restartSignature;
+    
     List<Exc> exceptions;
 
     ExcProcessor proc;
 
-
+    /**
+     * Main execution method for processing the log file.
+     * 
+     * @return An exit code; 0 indicates success, while 1 indicates failure (e.g., file not found).
+     * @throws Exception If an error occurs during file processing.
+     */
     @Override
     public Integer call() throws Exception {
         Path logPath = Path.of(logFile);
@@ -74,6 +91,7 @@ class exc implements Callable<Integer> {
 
         Charset charset = Charset.forName(encoding);
         String logText = Files.readString(logPath, charset);
+        
         extractExceptions(logText);
 
         if (outputHtml) {
@@ -102,21 +120,24 @@ class exc implements Callable<Integer> {
         
         return 0;
     }
-
-    // PrintStream:
-    // https://docs.oracle.com/javase/8/docs/api/java/io/PrintStream.html
-
-    void printExceptions(PrintStream out) {
-        for (Exc exc : exceptions) {
-            out.println("-");
-            out.println(exc.toYaml("  "));
-        }
-    }
-
+    
+    /**
+     * Extracts exceptions from the given log text.
+     * 
+     * @param logText The text content of the log file.
+     * @throws IOException If an I/O error occurs during processing.
+     */
     void extractExceptions(String logText) throws IOException {
         proc = new ExcProcessor();
+        if (restartSignature !=null) {
+            out.println("Restart signature: `" + restartSignature + "`");
+            proc.setRestartSignature(restartSignature);
+        }
+        
         TimestampExtractor tse = new SimpleTimestampExtractor();
         if (timeStampFormat != null) {
+            out.println("Timestamp format: `" + timeStampFormat + "`");
+            out.println("Skip prefix length: " + skipPrefix);
             tse.setDateFormat(timeStampFormat, skipPrefix);
         }
         exceptions = proc.process(logText, tse);
@@ -126,25 +147,55 @@ class exc implements Callable<Integer> {
                 //Date t1 = tse.parse(e1.time);
                 //Date t2 = tse.parse(e2.time);
                 //return -t1.compareTo(t2);
-                return Integer.compare(e2.lno, e1.lno);
+                return reverseOrder ? 
+                       Integer.compare(e2.lno, e1.lno) :
+                       Integer.compare(e1.lno, e2.lno);
             }
         });
     }
-
+    
+    /**
+     * Outputs the extracted exceptions to the specified {@link java.io.PrintStream} in YAML format.
+     * 
+     * @param out The PrintStream to output exceptions.
+     *
+     * @see https://docs.oracle.com/javase/8/docs/api/java/io/PrintStream.html
+     */
+    void printExceptions(PrintStream out) {
+        for (Exc exc : exceptions) {
+            out.println("-");
+            out.println(exc.toYaml("  "));
+        }
+    }
+    
+    /**
+     * The main method to execute the command-line application.
+     * 
+     * @param args Command-line arguments.
+     */
     public static void main(String... args) {
         int exitCode = new CommandLine(new exc()).execute(args);
         System.exit(exitCode);
     }
 
     // ------ Inner Classes 
-
+    
+    /**
+     * Represents an exception entry extracted from the log file.
+     */
     class Exc {
 
         public int lno;
         public String time;
         public String sig;
         public String cmt;
-
+        
+        /**
+         * Converts this exception entry to a YAML-formatted string.
+         * 
+         * @param indent The indentation to use for formatting.
+         * @return A YAML representation of the exception entry.
+         */
         public String toYaml(String indent) {
             StringBuilder sb = new StringBuilder();
             sb.append(indent + "lno: " + lno + "\n");
@@ -155,11 +206,24 @@ class exc implements Callable<Integer> {
         }
 
     }
-
+    
+    /**
+     * Processes log text to extract exceptions using a specified timestamp extractor.
+     */
     class ExcProcessor {
 
         int lno;
 
+        String restartSignature = "  :: Spring Boot ::  ";
+        
+        /**
+         * Processes the provided log text and extracts exceptions.
+         * 
+         * @param logText The log text to process.
+         * @param tse The TimestampExtractor to use for extracting timestamps.
+         * @return A list of extracted exceptions.
+         * @throws IOException If an I/O error occurs during processing.
+         */        
         public List<Exc> process(String logText, TimestampExtractor tse)
                 throws IOException {
             LinkedList<Exc> exceptions = new LinkedList<>();
@@ -180,7 +244,7 @@ class exc implements Callable<Integer> {
                     }
                     lastTime = tstamp;
 
-                    if (line.contains("  :: Spring Boot ::  ")) {
+                    if (line.contains(restartSignature)) {
                         exceptions.add(createExc("SPRING BOOT RESTART", tstamp, null));
                     }
                 }
@@ -202,7 +266,24 @@ class exc implements Callable<Integer> {
             in.close();
             return exceptions;
         }
-
+        
+        /**
+         * Sets the signature that indicates a restart event in the log.
+         * 
+         * @param restartSignature The signature string to identify restart events.
+         */        
+        void setRestartSignature(String restartSignature) {
+            this.restartSignature = restartSignature;
+        }
+        
+        /**
+         * Creates a new exception entry with the specified details.
+         * 
+         * @param sig The signature of the exception.
+         * @param time The timestamp when the exception occurred.
+         * @param cmt An optional comment associated with the exception.
+         * @return A new Exc object representing the exception.
+         */
         Exc createExc(String sig, String time, String cmt) {
             Exc exc = new Exc();
             exc.lno = lno;
@@ -214,7 +295,13 @@ class exc implements Callable<Integer> {
 
         private static final Pattern exceptionPattern = Pattern
                 .compile("(([a-z])+\\.)+[A-Z][a-zA-Z]*(Exception|Error)");
-
+                
+        /**
+         * Extracts the exception signature from a log line.
+         * 
+         * @param line The log line to process.
+         * @return The extracted exception signature, or null if none found.
+         */
         private String extractException(String line) {
             StringTokenizer st = new StringTokenizer(line);
             while (st.hasMoreTokens()) {
@@ -225,7 +312,12 @@ class exc implements Callable<Integer> {
             return null;
         }
 
-        /** Trims not only spaces but any non-letters. */
+        /**
+         * Trims special characters from the given token, keeping only letters.
+         * 
+         * @param token The token to trim.
+         * @return The trimmed token.
+         */
         String trimSpecial(String token) {
             int k1 = 0;
             int k2 = token.length() - 1;
@@ -241,7 +333,10 @@ class exc implements Callable<Integer> {
         }
 
     }
-
+    
+    /**
+     * Generates an HTML table from a list of exceptions in YAML format.
+     */
     class HtmlTable {
     
         // SnakeYAML: 
@@ -254,7 +349,13 @@ class exc implements Callable<Integer> {
         List<Map<String, Object>> list;    
         ArrayList<String> keys;
         String logFile;
-    
+        
+        /**
+         * Initializes an HtmlTable instance with the specified log file.
+         * 
+         * @param logFile The log file to read YAML data from.
+         * @throws Exception If an error occurs while reading the YAML file.
+         */    
         HtmlTable(String logFile) throws Exception {
             this.logFile = logFile;
             Path yamlPath = Path.of(logFile + ".yml");
@@ -263,10 +364,22 @@ class exc implements Callable<Integer> {
             //assert list.size() < 0;
         }
         
+        /**
+         * Creates an HTML output file from the YAML data.
+         * 
+         * @throws FileNotFoundException If the output file cannot be created.
+         */        
         void createOutputHtml() throws FileNotFoundException {
             createOutputHtml(logFile + ".html", list);
         }
-                
+        
+        /**
+         * Creates an HTML output file from the specified list of maps.
+         * 
+         * @param outFile The name of the output HTML file.
+         * @param list The list of data to include in the HTML table.
+         * @throws FileNotFoundException If the output file cannot be created.
+         */                
         void createOutputHtml(String outFile, List<Map<String, Object>> list) throws FileNotFoundException {
             Map<String, Object> map = list.get(0);
             keys = new ArrayList<>(map.keySet());
@@ -293,7 +406,12 @@ class exc implements Callable<Integer> {
             f.close();
             out.println("File created: " + outFile);
         }
-    
+        
+        /**
+         * Creates the contents of the HTML table from the extracted exception data.
+         * 
+         * @return A string containing the HTML table structure.
+         */    
         String createContents() {
             return String.format("""
                 <table class="table">
@@ -310,7 +428,12 @@ class exc implements Callable<Integer> {
                 createTableHeader(),
                 createTableBody());
         }
-    
+        
+        /**
+         * Creates the header row of the HTML table based on the keys of the data.
+         * 
+         * @return A string containing the HTML for the table header.
+         */    
         String createTableHeader() {
             StringBuilder sb = new StringBuilder();
             for (String key: keys) {
@@ -318,7 +441,12 @@ class exc implements Callable<Integer> {
             }
             return sb.toString();        
         }
-    
+        
+        /**
+         * Creates the body rows of the HTML table from the list of exceptions.
+         * 
+         * @return A string containing the HTML for the table body.
+         */    
         String createTableBody() {
             StringBuilder sb = new StringBuilder();
             for (Map<String, Object> em: list) {
@@ -339,22 +467,54 @@ class exc implements Callable<Integer> {
 
 // ------ Outer Classes 
 
+/**
+ * Interface for extracting timestamps and comments from log lines.
+ */
 interface TimestampExtractor {
-
+    
+    /**
+     * Extracts the timestamp from a log line.
+     * 
+     * @param line The log line to extract the timestamp from.
+     * @return The extracted timestamp as a string.
+     */
     String extractTimestamp(String line);
-
+    
+    /**
+     * Parses a timestamp string into a Date object.
+     * 
+     * @param tstamp The timestamp string to parse.
+     * @return The parsed Date object.
+     */
     Date parse(String tstamp);
-
+    
+    /**
+     * Gets the last extracted timestamp as a Date object.
+     * 
+     * @return The last extracted timestamp.
+     */
     Date getTimestamp();
-
+    
+    /**
+     * Extracts a comment from a log line.
+     * 
+     * @param line The log line to extract the comment from.
+     * @return The extracted comment as a string.
+     */
     String extractComment(String line);
-
+    
+    /**
+     * Sets the expected date format and the number of characters to skip before the timestamp.
+     * 
+     * @param dformat The date format string.
+     * @param skipPrefixLength The number of characters to skip.
+     */
     void setDateFormat(String dformat, int skipPrefixLength);
 
 }
 
 /**
- * Extract timestamp from the beginning of log line.
+ * Extracts timestamps from log lines using a predefined format.
  */
 class SimpleTimestampExtractor implements TimestampExtractor {
 
@@ -398,7 +558,13 @@ class SimpleTimestampExtractor implements TimestampExtractor {
     public Date getTimestamp() {
         return date;
     }
-
+    
+    /**
+     * Parses the given timestamp string into a Date object.
+     * 
+     * @param tstamp The timestamp string to parse.
+     * @return The parsed Date object, or a default date if parsing fails.
+     */
     public Date parse(String tstamp) {
         try {
             return df.parse(tstamp);
@@ -408,19 +574,21 @@ class SimpleTimestampExtractor implements TimestampExtractor {
     }
 
     /**
+     * Extracts a comment from a log line, if available.
+     *
      * The following code can be included into servlet filter
      * of the app to track which URL causes which exception.
      *
      * ```java
-    private void memoryInfo(HttpServletRequest request) {
-        long heapSize = Runtime.getRuntime().totalMemory();
-        long heapMaxSize = Runtime.getRuntime().maxMemory();
-        long heapFreeSize = Runtime.getRuntime().freeMemory();
-        String uri = request.getRequestURI();
-        log.info("[memoryInfo] uri=" + uri + ", free=" + heapFreeSize + ", total=" + heapSize + ", max=" + heapMaxSize);
-    }
-        * ```
-        */
+        private void memoryInfo(HttpServletRequest request) {
+            long heapSize = Runtime.getRuntime().totalMemory();
+            long heapMaxSize = Runtime.getRuntime().maxMemory();
+            long heapFreeSize = Runtime.getRuntime().freeMemory();
+            String uri = request.getRequestURI();
+            log.info("[memoryInfo] uri=" + uri + ", free=" + heapFreeSize + ", total=" + heapSize + ", max=" + heapMaxSize);
+        }
+     * ```
+     */
     @Override
     public String extractComment(String line) {
         if (line == null) {
